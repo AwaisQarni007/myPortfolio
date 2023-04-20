8233ffcc8f0d0f5cc060f784ce784af7b6e3f97f
Uchiha <official.qarni@gmail.com>
2023-04-20 12:09:39 +0500

    Adding Picture and changing some of content

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -71,7 +71,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/AwaisQarni007"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -81,7 +81,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="#"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -91,7 +91,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/muhammad-awais-qarni-22ba25218/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
