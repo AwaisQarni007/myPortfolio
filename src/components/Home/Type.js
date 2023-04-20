1beb63cfa636f1e8b18bef421cc7ebad7d79eff6
Uchiha <official.qarni@gmail.com>
2023-04-20 11:52:54 +0500

    first commit

---
+++ src/components/Home/Type.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Developer",
+          "Database Designer",
+          "Angular Learner",
+          "Software Engineer",
+          "MERN Stack Developer",
+          "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
